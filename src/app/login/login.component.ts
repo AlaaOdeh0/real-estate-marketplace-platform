51f3cf76c4769dedd7c5fe8a93407e95a10a2b5c
Alaa Odeh <alaaodeh16@gmail.com>
2025-04-13 21:08:26 +0300

    add login component with reactive forms and bootstrap styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login data:', this.loginForm.value);
+      // Call auth service here
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
