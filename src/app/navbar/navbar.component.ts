5b7d28c96ce667146847d6d28ec32c174c88f874
Alaa Odeh <alaaodeh16@gmail.com>
2025-04-13 15:43:32 +0300

    Add core components and routing setup: Home, Login, Registration, 404 page, Navbar, footer and routing for Buy, Sell, Rent, Home, Login, and Registration.

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router'; // ✅ Import here
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
