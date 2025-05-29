ee6a52481399e38fce5081500d661fe375fa6bf1
nooryaseen428 <s12323629@stu.najah.edu>
2025-05-29 17:11:47 +0300

    nav edite

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,12 +3,19 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { RegisterComponent } from '../register/register.component';
 import { NgIf } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
-
+import { NotificationBellComponent } from '../notification-bell/notification-bell.component'; // <-- هذا موجود صح!
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RegisterComponent, NgIf, LoginComponent],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RegisterComponent,
+    NgIf,
+    LoginComponent,
+    NotificationBellComponent // <--- ضيفيه هون 
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
